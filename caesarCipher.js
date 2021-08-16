6d9752b6f479fd59733b9e1459e03a2fa31ad7ab
k-mill1 <kaisamill@gmail.com>
2021-08-16 15:55:49 +0100

    add user key shift

---
+++ caesarCipher.js
@@ -0,0 +1,32 @@
+function enDeCode () {
+  const userInput = document.getElementById('userInput').value
+  const cleanString = userInput.toLowerCase()
+  const encode = document.getElementById('encode')
+  let userKey = Number(document.getElementById('userKey').value)
+  const userKeyShift = Number(document.getElementById('userKeyShift').value)
+  const outputMessage = []
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+  // iterates over user input, codes every letter and returns the output message
+  for (let i = 0; i < cleanString.length; i++) {
+    outputMessage.push(codeLetter(cleanString[i], userKey, encode))
+    if (alphabet.includes(cleanString[i])) {
+      userKey += userKeyShift
+    }
+  }
+
+  document.getElementById('output').value = outputMessage.join('')
+}
+// encodes/decodes letter passed into the function based on userkey value and returns the new letter
+function codeLetter (letter, userKey, encode) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  if (alphabet.includes(letter) && encode.checked) { // encodes
+    const position = alphabet.indexOf(letter)
+    const newPosition = (position + userKey) % 26
+    return alphabet[newPosition]
+  } else if (alphabet.includes(letter) && !encode.checked) { // decodes
+    const position = alphabet.indexOf(letter)
+    const newPosition = (position - userKey + 26) % 26
+    return alphabet[newPosition]
+  } else { return letter } // doesn't encode/decode non-letter characters
+}
